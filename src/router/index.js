d85b39d08f3a9ec379d67b357181f29c5e88479e
EROL KILBÜKER <erolposta@gmail.com>
2024-03-24 12:30:09 +0300

    fatura router

--- src/router/index.js
+++ src/router/index.js
@@ -20,9 +20,6 @@ const router = createRouter({
     {
       path: "/fatura",
       name: "fatura",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/Fatura.vue"),
     },
 
